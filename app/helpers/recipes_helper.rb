0ce6fa8f34621728ef3aaff124280d6ca5533deb
Bruno Felipe Iurczack <bruno.iurczack@outlook.com>
2023-02-07 11:35:19 -0300

    Criando helpers

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,2 +1,10 @@
 module RecipesHelper
+
+    def formata_caloria(recipe)
+        if recipe.Light?
+            content_tag(:strog, "Light (Menos de 100 calorias)")
+        else 
+            recipe.calories
+        end
+    end
 end
