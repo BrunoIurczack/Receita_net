b525e52f7258b8b16ee73d2fce58fecac7abfcaf
Bruno Felipe Iurczack <bruno.iurczack@outlook.com>
2023-02-08 16:27:26 -0300

    Criando Filtros atualizados

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,8 @@ class Recipe < ApplicationRecord
     def Light?
         calories < 100
     end
+
+    def self.created_at
+        where('created_at <= 7', time.now).order('created_at desc')
+    end
 end
