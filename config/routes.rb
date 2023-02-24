c845876f82c1c8fe0a2abff5f33b120377418cd4
Bruno Felipe Iurczack <bruno.iurczack@outlook.com>
2023-02-24 11:38:31 -0300

    Exibindo comentarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :comments
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :recipes
+  resources :recipes do 
+    resources :comments
+  end
 
   root 'recipes#index'
   get "recipes/index"
